11370b10edd41775baeaf16da8cc71a06b5710a2
franciscomiranda0 <franciscomiranda.inbox@gmail.com>
2022-09-18 21:00:47 -0300

    added constants file

---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,5 @@
+const String kAppTitle = 'Flutter Clean Architecture';
+const String kApiBaseUrl = 'https://newsapi.org/v2';
+const String kApiKey = '14356c0bd4ff4841ae628e815abfd26b';
+const String kArticlesTableName = 'articles_table';
+const String kDatabaseName = 'app_database.db';
