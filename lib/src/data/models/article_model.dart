65022b319875e26717c805caee0707cae1f2f788
franciscomiranda0 <franciscomiranda.inbox@gmail.com>
2022-09-21 22:23:02 -0300

    because flutter version was downgraded from 3.3.2 to 2.2.0, errors in models constructors were handled

--- lib/src/data/models/article_model.dart
+++ lib/src/data/models/article_model.dart
@@ -1,18 +1,29 @@
 import 'package:news_app/src/data/models/source_model.dart';
 import 'package:news_app/src/domain/entities/article.dart';
+import 'package:news_app/src/domain/entities/source.dart';
 
 class ArticleModel extends Article {
   const ArticleModel({
-    required super.id,
-    required super.source,
-    required super.title,
-    required super.description,
-    required super.content,
-    required super.author,
-    required super.url,
-    required super.urlToImage,
-    required super.publishedAt,
-  });
+    required int id,
+    required Source source,
+    required String title,
+    required String description,
+    required String content,
+    required String author,
+    required String url,
+    required String urlToImage,
+    required String publishedAt,
+  }) : super(
+          id: id,
+          author: author,
+          content: content,
+          description: description,
+          publishedAt: publishedAt,
+          source: source,
+          title: title,
+          url: url,
+          urlToImage: urlToImage,
+        );
 
   factory ArticleModel.fromMap(Map<String, dynamic> map) {
     return ArticleModel(
--- lib/src/data/models/source_model.dart
+++ lib/src/data/models/source_model.dart
@@ -2,9 +2,12 @@ import 'package:news_app/src/domain/entities/source.dart';
 
 class SourceModel extends Source {
   const SourceModel({
-    required super.id,
-    required super.name,
-  });
+    required String id,
+    required String name,
+  }) : super(
+          id: id,
+          name: name,
+        );
 
   factory SourceModel.fromMap(Map<String, dynamic> map) {
     return SourceModel(id: map['id'], name: map['name']);
