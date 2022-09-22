0f5ecff858ecb1e36daa363ecc682730cec88fc8
franciscomiranda0 <franciscomiranda.inbox@gmail.com>
2022-09-21 22:17:24 -0300

    removed tests folder as build_runner was having problem on a job; NewsApiService wip

---
+++ lib/src/data/datasources/remote/news_api_service.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:news_app/src/core/utils/constants.dart';
+import 'package:news_app/src/data/models/breaking_news_response_model.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'news_api_service.g.dart';
+
+@RestApi(baseUrl: kApiBaseUrl)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio, {required String baseUrl}) = _NewsApiService;
+
+  @GET('/top-headlines')
+  Future<HttpResponse<BreakingNewsResponseModel>> getBreakingNewsArticles({
+    @Query('apiKey') String apiKey,
+    @Query('country') String country,
+    @Query('category') String category,
+    @Query('page') String page,
+    @Query('pageSize') String pageSize,
+  });
+}
